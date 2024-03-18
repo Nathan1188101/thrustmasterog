5a26fc13890583b621248c370e759a90cb625c59
Nathan1188101 <njsecord@lakeheadu.ca>
2024-03-17 23:52:16 -0400

    create, edit, and delete all working for authorized users

--- Server/Controllers/content.js
+++ Server/Controllers/content.js
@@ -14,6 +14,40 @@ let index = async (req, res, next) => {
 
 }
 
+let displayCreateForm = async(req, res, next) => {
+    let content = await Content.find()
+
+    res.render('content/create',
+    {
+        title: 'Add New Content',
+        content: content,
+        user: req.user
+    })
+}
+
+let createContent = async(req, res, next) => {
+    await Content.create(req.body)
+
+    res.redirect('/content')
+}
+
+let displayEditForm = async(req, res, next) => {
+    let content = await Content.findById(req.params._id)
+
+    res.render('content/edit', {
+        title: 'Update',
+        content: content,
+        user: req.user 
+    })
+}
+
+let updateContent = async(req, res, next) => {
+    
+    await Content.findByIdAndUpdate(req.params._id, req.body)
+
+    res.redirect('/content')
+}
+
 let deleteContent = async (req, res, next) => {
 
     await Content.findByIdAndDelete(req.params._id)
@@ -24,5 +58,9 @@ let deleteContent = async (req, res, next) => {
 
 module.exports = {
     index,
+    createContent, 
+    displayCreateForm, 
+    displayEditForm, 
+    updateContent, 
     deleteContent
 }
--- Server/Routes/content.js
+++ Server/Routes/content.js
@@ -14,4 +14,20 @@ router.get('/delete/:_id', authCheck, (req, res, next) => {
     contentController.deleteContent(req, res, next)
 })
 
+router.get('/create', authCheck, (req, res, next) => {
+    contentController.displayCreateForm(req, res, next) 
+})
+
+router.post('/create', authCheck, (req, res, next) => {
+    contentController.createContent(req, res, next) 
+})
+
+router.get('/edit/:_id', authCheck, (req, res, next) => {
+    contentController.displayEditForm(req, res, next)
+})
+
+router.post('/edit/:_id', authCheck, (req, res, next) => {
+    contentController.updateContent(req, res, next) 
+})
+
 module.exports = router 
