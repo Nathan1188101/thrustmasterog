6795bd09330acf9a143bf43055ff0d2eccba2529
Nathan1188101 <njsecord@lakeheadu.ca>
2024-03-25 18:17:46 -0400

    doing work on edit page

--- Client/Scripts/search.js
+++ Client/Scripts/search.js
@@ -3,13 +3,13 @@ const content = document.getElementById('content').getElementsByTagName('li')
 
 let results = [] //empty array 
 
-searchInput.addEventListener("input", e => {
+searchInput.addEventListener("input", x => {
 
-    const value = e.target.value.toLowerCase()
+    const value = x.target.value.toLowerCase() //converting to lower case to get results always
     console.log(value) //for testing 
     Array.from(content).forEach(item => {
-        const title = item.querySelector('h5').textContent.toLowerCase()
-        item.style.display = title.includes(value) ? '' : 'none';
+        const title = item.querySelector('h5').textContent.toLowerCase() //getting the title and converting to lower 
+        item.style.display = title.includes(value) ? '' : 'none'; //if the title is included keep it visible other wise set to none 
     })
 })
 
