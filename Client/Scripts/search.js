8c883a4a03581a8c3bd39e9429b9027f5a380697
Nathan1188101 <njsecord@lakeheadu.ca>
2024-04-02 18:04:12 -0400

    commenting throughout code

--- Client/Scripts/search.js
+++ Client/Scripts/search.js
@@ -1,4 +1,7 @@
-const searchInput = document.getElementById('search')
+//search function code 
+
+//grabbing the elements by id on the content page
+const searchInput = document.getElementById('search') //in search bar
 const content = document.getElementById('content').getElementsByTagName('li')
 
 let results = [] //empty array 
@@ -7,9 +10,9 @@ searchInput.addEventListener("input", x => {
 
     const value = x.target.value.toLowerCase() //converting to lower case to get results always
     console.log(value) //for testing 
-    Array.from(content).forEach(item => {
+    Array.from(content).forEach(item => { 
         const title = item.querySelector('h5').textContent.toLowerCase() //getting the title and converting to lower 
-        item.style.display = title.includes(value) ? '' : 'none'; //if the title is included keep it visible other wise set to none 
+        item.style.display = title.includes(value) ? '' : 'none'; //if the title is included keep it visible other wise set to none and "hide it"
     })
 })
 
